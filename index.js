8a477d112f21689c0e27255a8904f4e4c9c9bed1
Karima El Hachimi <karima.elhachimi@portofantwerp.com>
2019-02-08 13:26:53 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const API_KEY = require('./api');
+
+
+const server = express();
+server.use(bodyParser.json());
+
+
+//end points
+
+server.post('/get-test-data/:data', (req, res) => {
+    const dataFromRequest = req.params.data;
+    console.log('request params: '+ req.params.data);
+    const dataToSend = `De data in de request was: ${dataFromRequest} en is goed ontvangen!`;
+    return res.json({
+        speech: dataToSend,
+        source: 'get-test-data'
+    });
+});
+
+server.listen((process.env.PORT || 8000), () => {
+    console.log("Server is up and running...");
+});
